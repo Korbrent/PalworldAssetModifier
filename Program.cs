c3d070874e8390a92d7527e706d6c572e1f364be
Kor <korbin.shelley@student.nmt.edu>
2025-03-01 17:56:45 -0700

    inital commit
    After getting everything to a working and satisfied state, I am ready to upload.
    
    I haven't yet gotten too much into Windows development, so I don't have too much prior experience with C#

---
+++ Program.cs
@@ -0,0 +1,437 @@
+﻿/***
+ * Author:          Korbrent
+ * Project:         PalworldAssetModifier
+ * Version:         V0.1.0
+ * Current Date:    3/1/2025
+ * First Date:      2/27/2025
+ * 
+ * Description:     Modify `.uasset` files using the UAssetAPI.
+ *
+ * See also:        https://pwmodding.wiki/docs/datatable-modding/uassetgui/UAssetGuide1
+ *                  https://atenfyr.github.io/UAssetAPI/index.html
+ */
+using UAssetAPI.ExportTypes;
+using UAssetAPI.PropertyTypes.Objects;
+using UAssetAPI.UnrealTypes;
+using UAssetAPI;
+using UAssetAPI.PropertyTypes.Structs;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace PalworldAssetModifier
+{
+    /**
+     * Config class.
+     * Loads in the config located at `./config.json` and maintains config information
+     */
+    class Config
+    {
+        // UnrealEngine version
+        public string engineVersion { get; set; }
+        // Amount to multiply cash by
+        public double cashMultiplier { get; set; }
+        // Amount to multiply expedition drops by
+        public double expeditionDropMultiplier { get; set; }
+        // Amount to chunk cash into so that it will only drop as a multiple of this number.
+        public int cashChunkAmount { get; set; }
+        // Cap for which min and maxNum abide by
+        public int softCap { get; set; }
+        // Amount to multiply weight of lvl 4 schematics by
+        public double weightMultiplier { get; set; }
+        // Min weight of lvl 4 schematics
+        public double minWeight { get; set; }
+        // Name table we are modifying
+        public string tableName { get; set; }
+        // Path to the `.usmap` file.
+        public string mappingPath { get; set; }
+        // Path to the FModel exports directory
+        public string exportsDir { get; set; }
+        // The asset path. This typically will be "/Pal/Content/Pal/..."
+        public string assetPath { get; set; }
+        // Name of asset to modify
+        public string assetName { get; set; }
+        // Directory to output new files
+        public string outputDir { get; set; }
+        // Automatically package after completion?
+        public bool doUnrealPak { get; set; }
+        // Directory to unrealPak (necessary only if doUnrealPak is true)
+        public string unrealPakDir { get; set; }
+
+        /**
+         * Load the config from the config file
+         */
+        static public Config LoadConfig()
+        {
+            string configPath = "config.json";
+            if (!File.Exists(configPath))
+            {
+                Console.WriteLine($"Config file not found! Expected at: {Path.GetFullPath(configPath)}");
+                return null;
+            }
+
+            string json = File.ReadAllText(configPath);
+            Config config = JsonConvert.DeserializeObject<Config>(json);
+            if (config is null)
+                return config;
+
+            // Trim any ending directory slashes. 
+            config.outputDir = config.outputDir.TrimEnd('\\', '/');
+            config.exportsDir = config.exportsDir.TrimEnd('\\', '/');
+            config.assetPath = config.assetPath.TrimEnd('\\', '/');
+            config.unrealPakDir = config.unrealPakDir.TrimEnd('\\', '/');
+            return config;
+        }
+    }
+
+    /**
+     * The main program class.
+     */
+    class Program
+    {
+        static private Config cfg = Config.LoadConfig();
+
+        /**
+         * Packs the outputDir using UnrealPak.exe located at unrealPakDir
+         * 
+         * Output file will be located at outputDir/..
+         * and will be named "{outputDirName}.pak"
+         */
+        static void unrealPak()
+        {
+            // Write filelist.txt
+            string filelistPath = Path.Join(cfg.unrealPakDir, "filelist.txt");
+            string fileContent = $"\"{cfg.outputDir}\\*.*\" \"..\\..\\..\\*.*\"";
+            File.WriteAllText(filelistPath, fileContent);
+            Console.WriteLine($"Filelist written at location: {filelistPath}");
+
+            // Run unrealPak
+            string unrealPakExe = Path.Join(cfg.unrealPakDir, "UnrealPak.exe");
+            string pakFile = $"{cfg.outputDir}.pak";
+            Console.WriteLine($"Creating pakfile {pakFile}");
+
+            // Create the process start info
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = unrealPakExe,
+                Arguments = $"\"{pakFile}\" -create=filelist.txt",
+                WorkingDirectory = cfg.unrealPakDir,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            // Run a process using the start info
+            using (Process process = new Process { StartInfo = psi })
+            {
+                process.OutputDataReceived += (sender, args) => Console.WriteLine(args.Data);
+                process.ErrorDataReceived += (sender, args) => Console.Error.WriteLine(args.Data);
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                process.WaitForExit();
+            }
+        }
+
+        /**
+         * Writes out the new asset files to the outputDir
+         * If outputDir does not exist, then it will create it.
+         */
+        static void writeOut(UAsset asset)
+        {
+            string outputDir = cfg.outputDir,
+                assetPath = cfg.assetPath;
+            if (outputDir.TrimEnd('\\', '/').EndsWith(assetPath, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine($"outputDir contained assetPath, removing assetPath from outputDir.\n\tOutputDir:{outputDir}");
+                outputDir = outputDir.Substring(0, outputDir.Length - assetPath.Length).TrimEnd('\\', '/');
+                Console.WriteLine($"New outputDir: {outputDir}");
+                cfg.outputDir = outputDir;
+            }
+
+            if (!Directory.Exists(outputDir))
+            {
+                Console.WriteLine($"outputDir does not exist. Creating it now at {outputDir}");
+                Directory.CreateDirectory(outputDir);
+            }
+
+            if (!Directory.Exists(Path.Join(outputDir, assetPath)))
+            {
+                Console.WriteLine($"outputDir/assetPath does not exist. Creating it now at {Path.Join(outputDir, assetPath)}");
+                Directory.CreateDirectory(Path.Join(outputDir, assetPath));
+            }
+
+            string output = Path.Join(outputDir, assetPath, cfg.assetName);
+            asset.Write(output);
+            Console.WriteLine("File written to " + output);
+        }
+
+        /**
+         * Modifies the droprate of a Blueprint item
+         * 
+         * Modifying droprate is as simple as changing the WeightInSlot of an item.
+         * 
+         * Each Grade of chest has several Slots in them.
+         * Each slot drops an item, where the item's drop rate in that slot is p[item] = (item.WeightInSlot / slot.TotalWeight)
+         * Calculating the chance to get an item in a chest is as simple as multiplying (1 - p[item]) for each slot, then subtracting 1 by the resulting value
+         */
+        static void modifyBP(StructPropertyData e, string itemName, string fieldName)
+        {
+            double weight_old = -1,
+                weight_new = -1,
+                weightMultiplier = cfg.weightMultiplier,
+                minWeight = cfg.minWeight;
+            PropertyData prop;
+
+            Console.Write(String.Format("EntryName: {0:S}\tFieldName: {2:S}\tItemID: {1:S}", e.Name, itemName, fieldName));
+            //"\tProperty Type:" + e.PropertyType + "\tStructType: " + e.StructType);
+
+            // WeightInSlot will be a FloatProperty, as we can see in UAssetGUI. 
+            prop = e["WeightInSlot"];
+            if (prop is FloatPropertyData floatprop)
+            {
+                weight_old = (double)floatprop.Value;
+                floatprop.Value *= (float)weightMultiplier;
+                weight_new = (double)floatprop.Value;
+            }
+            // This next block is useless, however it is here for sanity checking.
+            else if (prop is DoublePropertyData doubleprop)
+            {
+                weight_old = doubleprop.Value;
+                doubleprop.Value *= weightMultiplier;
+                weight_new = doubleprop.Value;
+            }
+
+            if ((weight_new < minWeight) && (minWeight > 0))
+            {
+                if (prop is FloatPropertyData fp)
+                    fp.Value = (float)minWeight;
+                if (prop is DoublePropertyData dp)
+                    dp.Value = minWeight;
+                weight_new = minWeight;
+            }
+
+            Console.Write(String.Format("\tWeight: {0:F2} -> {1:F2} ({2:F2}%)\n", weight_old, weight_new, ((weight_new - weight_old) / weight_old) * 100));
+        }
+
+        /**
+         * Modifies a row by adjusting the minNum and maxNum to increase amount of drops
+         * 
+         * This does not increase the actual drop rate, only the quantity that drops.
+         * Modifying the drop rate is as simple as changing the WeightInSlot parameter.
+         */
+        static void modifyOther(StructPropertyData e, string itemName, string fieldName, double mult, int chunk)
+        {
+            PropertyData prop;
+            int val;
+            int softCap = cfg.softCap;
+
+            prop = e["MinNum"];
+            if (!(prop is IntPropertyData))
+            {
+                Console.WriteLine("MinNum: " + prop.PropertyType);
+                Environment.Exit(5);
+            }
+            val = (int)(((IntPropertyData)prop).Value * mult);
+            ((IntPropertyData)prop).Value = (val < softCap ? val : softCap);
+
+            prop = e["MaxNum"];
+            if (!(prop is IntPropertyData))
+            {
+                Console.WriteLine("MaxNum: " + prop.PropertyType);
+                Environment.Exit(5);
+            }
+            val = (int)(((IntPropertyData)prop).Value * mult);
+            ((IntPropertyData)prop).Value = (val < softCap ? val : softCap);
+
+            prop = e["NumUnit"];
+            if (!(prop is IntPropertyData))
+            {
+                Console.WriteLine("NumUnit: " + prop.PropertyType);
+                Environment.Exit(5);
+            }
+            if (fieldName.StartsWith("Expedition"))
+            {
+                // Multiply for expeditions
+                val = (int)(((IntPropertyData)prop).Value * mult);
+                ((IntPropertyData)prop).Value = (val < softCap ? val : softCap);
+            }
+            else if (chunk > 0)
+            {
+                // Set to chunk amount if set (for cash)
+                val = chunk;
+                ((IntPropertyData)prop).Value = (val < softCap ? val : softCap);
+            }
+            Console.Write(String.Format("EntryName: {0:S}\tFieldName: {2:S}\tItemID: {1:S}", e.Name, itemName, fieldName));
+            Console.WriteLine("\tValues modded");
+            return; // Next row
+        }
+
+        /**
+         * Filters based on fieldNames.
+         * See internal comment for why this function exists.
+         */
+        static bool fieldNameFilter(string fieldName)
+        {
+            /* The mod crashes if the value of [min,max]Num is too high. 
+             * I can't find an exact upper limit for it.
+             * Disabling the following fields seems to help a tad.
+             *  -   Without filtering, the mod would crash at a softCap of 25000 and sometimes at 15k
+             *  -   With filtering, the mod would sometimes crash at a softCap of 25000 but safe at 15k
+             *  
+             * I dont see a benefit in wasting more time debugging this or finding exactly what the limit is before crashing.
+             * I have spent a lot of time with trial-and-error and discovered a soft-cap of ~15k is safe.
+             */
+            List<String> fieldNameFilters = new List<String>();
+            fieldNameFilters.Add("Dev");
+            fieldNameFilters.Add("CharacterSpawn");
+
+            bool passedTest = true;
+            foreach (String filter in fieldNameFilters)
+            {
+                if (fieldName.StartsWith(filter))
+                {
+                    Console.WriteLine("Skipping due to filter");
+                    passedTest = false;
+                    break;
+                }
+            }
+            return passedTest;
+        }
+
+        /**
+         * Handle a single row of data from our table
+         */
+        static void handleRow(StructPropertyData e)
+        {
+            // Get the property (which is like a cell in the table) that corresponds to this row's StaticItemId
+            // We can see in UAssetGUI that this is supposed to be a NameProperty
+            PropertyData prop = e["StaticItemId"];
+            String itemName = null;
+            if (prop is NamePropertyData)
+            {
+                itemName = ((NamePropertyData)prop).Value.ToString();
+            }
+            if (itemName is null)
+                return; // Next row
+
+            // Get the FieldName property (same as above)
+            prop = e["FieldName"];
+            String fieldName = "N/A";
+            if (prop is NamePropertyData)
+            {
+                fieldName = ((NamePropertyData)prop).Value.ToString();
+            }
+
+            // I mostly just want to increase the droprates of Lvl 4 schematics. 
+            if ((itemName.EndsWith("_5") && itemName.StartsWith("Blueprint")))
+            {
+                modifyBP(e, itemName, fieldName);
+                return;
+            }
+
+            // I want to increase the amount of Money and DogCoin drops though, 
+            // as well as the drops from expeditions. So I will check if the ItemName is currency or if the Field is an Expedition
+            if (!((itemName.Equals("Money") || itemName.Equals("DogCoin")) || fieldName.StartsWith("Expedition")))
+                return; // Next row
+
+            if (fieldNameFilter(fieldName))
+                return;
+
+            // If this row is an Expedition item drop, we want to increase the drops.
+            if (fieldName.StartsWith("Expedition"))
+            {
+                modifyOther(e, itemName, fieldName, cfg.expeditionDropMultiplier, -1);
+            }
+            else
+            {
+                modifyOther(e, itemName, fieldName, cfg.cashMultiplier, cfg.cashChunkAmount);
+            }
+            return;
+
+        }
+
+        static void Main(string[] args)
+        {
+            if (cfg is null)
+            {
+                Environment.Exit(1);
+            }
+
+            // We can't have a negative multiplier, only valid input is [0, inf)
+            if (cfg.cashMultiplier < 0 || cfg.expeditionDropMultiplier < 0 || cfg.weightMultiplier < 0)
+            {
+                Console.WriteLine("Invalid multiplier value (leq zero)");
+                Environment.Exit(1);
+            }
+
+            // minWeight or cashChunkAmount leq zero means to ignore. Doesnt work for multipliers (they should use 1.0 instead, since x * 1 = x)
+            if (cfg.softCap <= 0)
+            {
+                Console.WriteLine("Invalid softCap value. Trust me, you want this to be set if you are dealing with large numbers. I've gone ahead and set it to 15000 for you.");
+                cfg.softCap = 1500;
+            }
+
+            if (!Enum.TryParse(cfg.engineVersion, out EngineVersion engine))
+            {
+                Console.WriteLine($"Invalid engine version specified in config: {cfg.engineVersion}");
+                Environment.Exit(2);
+            }
+
+            string tableName = cfg.tableName,
+                mappingPath = cfg.mappingPath,
+                assetDir = (cfg.exportsDir.Contains(cfg.assetPath) ? cfg.exportsDir : Path.Join(cfg.exportsDir, cfg.assetPath)),
+                assetName = cfg.assetName,
+                outputDir = cfg.outputDir;
+
+            if (!assetDir.EndsWith(assetName))
+                assetDir = Path.Join(assetDir, assetName);
+
+            Console.WriteLine("Starting...");
+
+            // Instantiate the asset with the path, engine version, and mapping. 
+            UAsset myAsset = new UAsset(assetDir, engine, new UAssetAPI.Unversioned.Usmap(mappingPath));
+
+            // Since there is only 1 Export in the list, we could just do `DataTableExport itemLottery_DT = (DataTableExport)(myAsset.Exports[0])` but this way is more "fool proof"
+            DataTableExport itemLottery_DT = null;
+            List<Export> exportsList = myAsset.Exports;
+            foreach (Export export in exportsList)
+            {
+                if (tableName.Equals(export.ObjectName.ToString()))
+                {
+                    Console.WriteLine("Found the correct table");
+                    if (!(export is DataTableExport))
+                    {
+                        Console.WriteLine($"Unexpected type for the DataTable: {export.GetExportClassType()}");
+                        Environment.Exit(3);
+                    }
+                    itemLottery_DT = (DataTableExport)export;
+                }
+            }
+
+            if (itemLottery_DT is null)
+            {
+                Console.WriteLine("Error: DataTable is null.");
+                Environment.Exit(4);
+            }
+
+            // Get the table data from the datatable
+            List<StructPropertyData> entries = itemLottery_DT.Table.Data;
+
+            // Iterate through each row in the table
+            foreach (StructPropertyData e in entries)
+            {
+                handleRow(e);
+            }
+
+            writeOut(myAsset);
+
+            if (cfg.doUnrealPak)
+            {
+                Console.WriteLine("Automatic packaging enabled");
+                unrealPak();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # PalworldAssetModifier
-Uses the UAssetAPI to modify datatables in Palworld. (Though this can be reapplied to work for any Unreal Engine game)
+Uses the UAssetAPI to modify datatables in Palworld. 
+
+# Requirements:
+- `UAssetAPI` - [atenfyr/UAssetAPI](https://github.com/atenfyr/UAssetAPI)
+    - See [https://atenfyr.github.io/UAssetAPI/guide/basic.html](https://atenfyr.github.io/UAssetAPI/guide/basic.html) for further information on how to set it up.
+- `FModel` - [fmodel.app](https://fmodel.app/)
+    - See [https://pwmodding.wiki/docs/asset-swapping/StartingOut](https://pwmodding.wiki/docs/asset-swapping/StartingOut) for further information on how to set it up.
+## Optional:
+- `UnrealPak`
+    - This seems to be downloadable from many locations, though I downloaded mine from [here](https://github.com/RiotOreO/unrealpak).
+
+# How to use:
+
+To use this program:
+- First, set up your config file. (See the "**Setting up the config**" section).
+- After the config has been set up to your liking, run `PalworldAssetModifier.exe`.
+- The files will be output to your configured `outputDir`.
+- If automatic packing was enabled, the `.pak` file will be located in the same directory as `outputDir`. 
+
+## Setting up the config
+The config should be named `config.json` and located in the same location as the `.exe` program. An `exampleConfig.json` has been provided. 
+- Open the config and modify the following:
+    - `exportsDir` : The directory of your FModel exports file.
+        - *Ex* : `D:\\PalworldModding\\Tools\\FModel\\Output\\Exports`
+        - *Note* : Assuming you followed the instructions to set up FModel, then see [here](https://pwmodding.wiki/docs/datatable-modding/uassetgui/UAssetGuide1#step-1-exporting-the-file-we-want-to-edit) about generating the Exports.
+    - `outputDir` : The directory to output to. If this directory doesn't exist, it will be created.
+        - *Ex* : `D:\\PalworldModding\\Out\\ExampleMod_P`
+    	- *Note* : This should be the top level directory. This should not include the subdirectory asset path. 
+    - `mappingPath` : The path to the `.usmap` mapping file.
+        - *Ex* : `D:\\PalworldModding\\Mappings.usmap`
+        - If you do not have this file, it can be obtained from [here](https://github.com/PalworldModding/UsefulFiles/raw/refs/heads/master/Mappings.usmap).
+
+- If you want it to automatically pack your mod into a `.pak` file...
+    - `doUnrealPak` : This should be true if you want automated packing.
+    - `unrealPakDir` : This should be the directory that `UnrealPak.exe` is located in.
+        - *Ex* : `D:\\PalworldModding\\Tools\\UnrealPak`
+    - *Note* : The `.pak` file will be located in the same directory as your `outputDir`.
+        - *Ex* : If `outputDir` is `D:\\PalworldModding\\Out\\ExampleMod_P`, then the `.pak` file will be located at `D:\\PalworldModding\\Out\\ExampleMod_P.pak`.
+
+- The following values should be modified if you intend to adjust the mods drop rates and multipliers:
+    - `weightMultiplier` : The multiplier value to adjust Lvl 4 blueprints to.
+        - Every chest "Grade" in Palworld has several "Slots", each Slot contains multiple items, each item with their own individual weight in that Slot. The probability of an item to drop from a Slot is thus equivalent to: `item.WeightInSlot / slot.TotalWeight`.
+        - The default weight values and drop chances can be found [here](https://palworld.fandom.com/wiki/Treasure_Chest).
+        - If you wanted to calculate the probability of a specific item dropping from a chest Grade, then you could do the following:
+            - Multiply the probability of failure for each slot, we'll call this `q_tot` (recall: `q = 1 - p` from basic statistics).
+            - This gives you the total chance to fail across slots, thus the chance to succeed across at least one slot is `1 - q_tot`.
+            - Here's a pseudocode equivalent: `p_fail = 1; for Slot in Grade: p_fail *= (1 - Slot.item.p_success); Grade.item.p_success = 1 - p_fail`
+            - Thank you for attending my Basic Probabilities lecture.
+    - `minWeight` : The minimum weight value to set level 4 blueprints to. 
+        - If the weight of the blueprint after the multiplier is less than this value, it will be bumped up to this value.
+    - `expeditionDropMultiplier` : The amount to multiply expedition drops by.
+    - `cashChunkAmount` : The amount to chunk cash drops into. (This includes DogCoin and Money.)
+        - Setting this to `-1` will leave this parameter alone.
+        - This doesn't change the min or max amount earned from chests.
+    - `cashMultiplier` : The amount to multiply cash drops by.
+    - `softCap` : If any drop multipliers result in a value higher than this, it will be set to this.
+        - *Note* : Necessary to avoid crashing, as too high of a quantity of drops will result in crashing.
+
+- Optionally, you can modify the following, though it should not be necessary unless you intend to change the functionality of the program:
+    - `assetPath` : This should be the subdirectory structure to the asset.
+        - *Ex* : `\\Pal\\Content\\Pal\\DataTable\\Item`
+        - *Note* : 99% of the time, this will start with `Pal\\Content\\Pal\\DataTable` and you will only be changing the last directory.
+    - `assetName` : The name of the `.uasset` file to modify.
+        - *Ex* : `DT_ItemLotteryDataTable.uasset`
+        - *Note* : As described [here](https://pwmodding.wiki/docs/datatable-modding/uassetgui/UAssetGuide1), if the file has an `_Common` variant, that one should be used.
+    - `tableName` : The name of the table to modify. This is typically the same as the assetName without ".uasset" at the end.
+
+## Setting up the project 
+If you intend to modify the program, you must follow these steps.
+However, if you do not plan on changing the code and only want to adjust the mod's drop rates, you can skip this section.
+- Create a new "C# Console App" in Visual Studio with the Framework set to ".NET 8.0"
+- In the "Solution Explorer" tab, right-click on "Dependencies", add a project reference, and set the location to the `UAssetAPI.dll`.
+- See [the UAssetAPI guide](https://atenfyr.github.io/UAssetAPI/guide/basic.html) for further help on the previous steps.
+- Use my `Program.cs` file.
+- Enjoy messing with the code. I don't often program in C#, so I am certain I have committed a couple language-specific sins.
+
+# See More:
+Refer to the following for more information:
+- [Palworld Modding wiki guide on UAsset modding](https://pwmodding.wiki/docs/datatable-modding/uassetgui/UAssetGuide1)
+- [UAssetAPI Docs](https://atenfyr.github.io/UAssetAPI/index.html)
+
+Feel free to contact me on Discord if you need further help.
